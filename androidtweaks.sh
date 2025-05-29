431679e5a6eb31c3d1babbe821cb18b45c99924d
shoober420 <shoober420@gmail.com>
2025-05-29 13:16:26 -0700

    Update androidtweaks.sh

--- androidtweaks.sh
+++ androidtweaks.sh
@@ -57,7 +57,7 @@ setprop debug.hwui.use_gpu_pixel_buffers true
 setprop debug.hwui.render_dirty_regions false
 setprop debug.hwui.force_refresh_rate 120
 setprop debug.hwui.refresh_rate_forced 120
-setprop debug.heui.refresh_rate 120
+setprop debug.hwui.refresh_rate 120
 setprop debug.hwui.render_dirty_regions false
 setprop debug.sf.hw true
 setprop debug.egl.force_msaa true
@@ -74,7 +74,38 @@ setprop debug.hwui.force.gpu true
 setprop debug.hwui.force_gpu true
 setprop debug.egl.msaa-sample 16
 setprop debug.sf.msaa 16
+setprop debug.hwui.use_hint_manager true
+setprop debug.egl.hw 1
+setprop debug.egl.swapinterval 0
+setprop debug.gl.hw 1
+setprop debug.gl.swapinterval 0
+setprop debug.gr.swapinterval 0
+setprop debug.dev.ssrm.turbo true
+setprop debug.rs.max-threads true
+setprop debug.rs.precision true
+setprop debug.sf.enable_hwc_vds true
+setprop debug.mdpcomp.logs 0
+setprop persist.hwc.mdpcomp.enable true
+setprop persist.hwc.ptor.enable true
+setprop vendor.gralloc.enable_fb_ubwc 1
+setprop debug.sf.disable_hwc 0
+setprop debug.sf.disable_backpressure 1
+setprop debug.sf.gpu_comp_tiling 1
 
+setprop audio.deep_buffer.media true
+setprop audio.offload.disable true
+setprop ro.vendor.audio.sdk.ssr false
+setprop vendor.audio.flac.sw.decoder.24bit true
+setprop vendor.audio.offload.multiple.enabled false
+setprop vendor.audio.playback.mch.downsample false
+setprop vendor.audio.pp.asphere.enabled false
+setprop vendor.audio.safx.pbe.enabled false
+setprop vendor.audio.tunnel.encode false
+setprop vendor.audio.use.sw.alac.decoder true
+setprop vendor.audio.use.sw.ape.decoder true
+setprop vendor.audio.hw.aac.encoder true
+
+setprop persist.qfp false
 setprop ro.sf.blurs_are_expensive 0
 setprop ro.surface_flinger.supports_background_blur 0
 
